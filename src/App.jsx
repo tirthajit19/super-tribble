358e811b712c4fc4d6fc29e5263c33251887fc5e
Tirthajit Das <tirthajitdas@Tirthajits-MacBook-Pro.local>
2024-07-07 15:02:54 +0530

    Added new CoreConcepts component and passed props from data.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import reactImage from "./assets/react-core-concepts.png"
+import { CORE_CONCEPTS } from "./data.js"
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+const CoreConcepts = ({image, title, description}) => {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
 }
 
 function Header() {
@@ -24,7 +35,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
