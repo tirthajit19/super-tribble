947424c93bfe6525fe08bdf19a57218b1f00fe8b
Tirthajit Das <tirthajitdas@Tirthajits-MacBook-Pro.local>
2024-07-08 22:00:47 +0530

    Moved the contents of the components from app.jsx to dedicated component files

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,9 @@
+export const CoreConcepts = ({image, title, description}) => {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
