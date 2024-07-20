06ade956bfebd093a714feec545d63cd77dee0f7
Tirthajit Das <tirthajitdas@Tirthajits-MacBook-Pro.local>
2024-07-20 18:20:58 +0530

    Added active status to class when buttons are clicked and also used map function to have clean code approach to read the concepts data from data.js file

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export const TabButton = ({ children, onSelect }) => {
+export const TabButton = ({ children, onSelect, isSelected }) => {
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 };
